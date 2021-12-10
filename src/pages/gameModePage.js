279ccd9bd146c4dc35724799f0d113e2fdbd843f
margie <margie@ais.pl>
2021-12-10 21:12:22 +0100

    add quiz houses page

--- src/pages/gameModePage.js
+++ src/pages/gameModePage.js
@@ -1,8 +1,13 @@
+import mapNavigationClickToTemplate from '../navigation';
+import createQuizHousesPage from './quizHousesPage';
+
 const createGameModePage = (rootElement) => {
   const appScreen = document.querySelector(rootElement);
   const gameModePage = document.querySelector('#gameModePage');
 
   appScreen.innerHTML = gameModePage.innerHTML;
+
+  mapNavigationClickToTemplate(rootElement, '[data-action-houses]', createQuizHousesPage);
 };
 
 export default createGameModePage;
