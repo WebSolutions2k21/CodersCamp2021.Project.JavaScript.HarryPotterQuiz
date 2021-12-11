a62042cbc291a3cfa43c1e0be1c99efa245a1de9
Marta Probierz <marta.anna.probierz@gmail.com>
2021-12-11 20:12:24 +0100

    Corrections

--- src/pages/gameModePage.js
+++ src/pages/gameModePage.js
@@ -7,7 +7,25 @@ const createGameModePage = (rootElement) => {
 
   appScreen.innerHTML = gameModePage.innerHTML;
 
-  mapNavigationClickToTemplate(rootElement, '[data-action-houses]', createQuizHousesPage);
+  //mapNavigationClickToTemplate(rootElement, '[data-action-houses]', createQuizHousesPage);
+
+
+  // change placeholder 
+  const form = document.querySelector("form");
+  form.addEventListener("focus", function( event ) {
+    event.target.style.background = "rgba(211, 166, 37, 1)";
+  }, true);
+  form.addEventListener("blur", function( event ) {
+    event.target.style.background = "";
+  }, true);
+ 
+  
+  // add border color 
+  document.addEventListener("click", function(event){
+    if(event.target.classList.contains("gameMode__btn")){
+      event.target.classList.toggle("bor")  
+    }
+  });
 };
 
 export default createGameModePage;
