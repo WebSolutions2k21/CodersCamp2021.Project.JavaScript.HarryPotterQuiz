9e099adec7a891dc28b1e660bcdb8e038fe267e8
Anna Koruc <aniakoruc@gmail.com>
2021-12-13 21:22:07 +0100

    Add temporary localStorage + sorting players by score

--- src/pages/resultPage.js
+++ src/pages/resultPage.js
@@ -15,3 +15,25 @@ const createResultPage = (rootElement) => {
 };
 
 export default createResultPage;
+
+// // tymczasowy local Storage
+localStorage.setItem('Karol', 500);
+localStorage.setItem('Basia', 200);
+localStorage.setItem('Celina', 250);
+localStorage.setItem('Mateusz', 700);
+localStorage.setItem('Iza', 550);
+localStorage.setItem('Kuba', 150);
+localStorage.setItem('Patryk', 100);
+
+const players = [];
+
+for (let [key, value] of Object.entries(localStorage)) {
+  players.push([key, Number(value)]);
+}
+
+for (const [key, value] of Object.entries(players)) {
+  console.log(key, value);
+}
+players.sort((a, b) => b[1] - a[1]);
+
+console.log(players);
