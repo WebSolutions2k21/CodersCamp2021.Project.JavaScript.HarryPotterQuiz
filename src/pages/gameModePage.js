7e1f115551dc070821fc8bd467fb7a23c335ab8c
SolWika <solwika@github.com>
2021-12-17 15:23:17 +0100

    Saving player data in local storage

--- src/pages/gameModePage.js
+++ src/pages/gameModePage.js
@@ -8,6 +8,8 @@ const createGameModePage = (rootElement) => {
 
   appScreen.innerHTML = gameModePage.innerHTML;
 
+  addEventListenersForGameModeButtons();
+
   mapNavigationClickToTemplate(rootElement, '[data-action-houses]', createQuizHousesPage);
   mapNavigationClickToTemplate(rootElement, '[data-action-exit]', createHomePage);
 
@@ -47,14 +49,34 @@ const createGameModePage = (rootElement) => {
   tipBtn.forEach((btn) => {
     btn.addEventListener('click', (event) => {
       tipBtn.forEach((btn) => {
+
         btn.classList.remove('bor');
+
         if (event.target.innerHTML == btn.innerHTML) {
           btn.classList.add('bor');
         }
       });
     });
   });
+
 };
+
+function addEventListenersForGameModeButtons() {
+
+  document.querySelectorAll('[data-action-button]').forEach(element => {
+    element.addEventListener('click', savePlayerName);
+  });
+
+  // var buttons = document.querySelectorAll('[data-action-button]');
+  // for (var i = 0; i < buttons.length; i++) {
+  //   buttons[i].addEventListener('click', savePlayerName);
+  // }
+};
+
+function savePlayerName() {
+  const login = document.querySelector('#fname').value;
+  localStorage.setItem("namePlayer", login);
+}
 
 
 export default createGameModePage;
