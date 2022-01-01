57fc73c0bd4461edc485ec8f792d976d723f9f2d
WindOfCodes <sebastian.michalczyk95@gmail.com>
2022-01-01 10:54:30 +0100

    quizHouse mobile

---
+++ src/timer.js
@@ -0,0 +1,21 @@
+
+const startingMinutes = 1;
+let time = startingMinutes * 60;
+
+const countDownEl = document.getElementById('timer_clock');
+
+function updateCountDown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countDownEl.innerHTML = `0${minutes}:${seconds}`;
+    time--;
+
+    if(seconds == '01') {
+        window.location = '/result';
+    }
+}
+
+setInterval(updateCountDown, 1000);
