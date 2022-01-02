bd2dfb0f802740d0cd9e10a3ebee7ea69947d71a
Aleksander Atamańczuk <56917808+TenGosc007@users.noreply.github.com>
2022-01-02 14:54:28 +0100

    Revert "Aleksander"

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -1,33 +1,11 @@
-import i18next from '../i18n';
 import mapNavigationClickToTemplate from '../navigation';
 import { paths } from '../shared/router';
 
 const createHomePage = () => {
   const appScreen = document.querySelector('#root');
   const homePage = document.querySelector('#homePage');
-  const { t, changeLanguage } = i18next;
 
   appScreen.innerHTML = homePage.innerHTML;
-
-  document.querySelector('[data-action-start]').innerText = t('start');
-  document.querySelector('[data-action-rules]').innerText = t('rules');
-  document.querySelector('[data-action-scores]').innerText = t('best_scores');
-  const container = document.querySelector('.wrapperHomePage ');
-  const fragment = document.createElement('div');
-  fragment.innerHTML = `
-  <button id="en" class="btn" style="padding: 1rem">en</button>
-  <button id="pl" class="btn" style="padding: 1rem">pl</button>
-  `;
-  container.appendChild(fragment);
-  document.getElementById('en').addEventListener('click', () => {
-    changeLanguage('en');
-    window.location.reload();
-  });
-
-  document.getElementById('pl').addEventListener('click', () => {
-    changeLanguage('pl');
-    window.location.reload();
-  });
 
   mapNavigationClickToTemplate('[data-action-start]', paths.gameMode);
   mapNavigationClickToTemplate('[data-action-rules]', paths.rules);
