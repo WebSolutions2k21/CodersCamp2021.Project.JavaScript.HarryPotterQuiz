3b0c22a9fb3c8b46264c9d8ccab1755e18cd18f8
Marta Probierz <marta.anna.probierz@gmail.com>
2022-01-03 11:33:54 +0100

    fix the timer

--- src/timer.js
+++ src/timer.js
@@ -1,21 +1,23 @@
 const timer = () => {
-    const startingMinutes = 1;
-    let time = startingMinutes * 60;
-    const countDownEl = document.getElementById('timer_clock');
-    function updateCountDown() {
-        const minutes = Math.floor(time / 60);
-        let seconds = time % 60;
-
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-
-        countDownEl.innerHTML = `0${minutes}:${seconds}`;
-        time--;
-
-        if(seconds == '01') {
-          window.location = '/result';
-        }
-    }
-    setInterval(updateCountDown, 1000);
-}
-
-export default timer;
+      const startingMinutes = 1;
+      let time = startingMinutes * 60;
+      const countDownEl = document.querySelectorAll('.timer_clock');
+      for (let i = 0; i < countDownEl.length; i++) {
+        function updateCountDown() {
+          const minutes = Math.floor(time / 60);
+          let seconds = time % 60;
+  
+          seconds = seconds < 10 ? '0' + seconds : seconds;
+  
+          countDownEl[i].innerHTML = `0${minutes}:${seconds}`;
+          time--;
+  
+          if(seconds == '01') {
+            window.location = '/result';
+          }
+      }
+      setInterval(updateCountDown, 1000);
+      }
+  }
+  
+  export default timer;
