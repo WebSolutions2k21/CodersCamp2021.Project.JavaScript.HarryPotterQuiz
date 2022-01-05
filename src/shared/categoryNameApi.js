42c067bc2a6cf4f49a429137dc3672ffa454a119
kfilip <katarzyna.filip87@gmail.com>
2022-01-05 18:42:08 +0100

    build

--- src/shared/categoryNameApi.js
+++
@@ -1,7 +0,0 @@
-const categoryName = {
-  API_CHARACTERS_STUDENTS: 'api/characters/students',
-  API_CHARACTERS_STAFF: 'api/characters/staff',
-  API_CHARACTERS_HOUSES: 'api/characters',
-};
-
-export default categoryName;
--- test/setupJest.js
+++
@@ -1,4 +0,0 @@
-import 'regenerator-runtime/runtime'; // async/await with Parcel
-import fetchMock from 'jest-fetch-mock';
-
-fetchMock.enableMocks();
